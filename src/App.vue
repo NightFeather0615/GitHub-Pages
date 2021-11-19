55c85f0d6fc70b346f1aee5d40ca289793167ef3
NightFeather <leolee50910@gmail.com>
2021-11-20 01:46:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Home from './components/Home.vue'
+</script>
+
+<template>
+  <Home class=""/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+body {
+  background-color: rgb(21, 24, 29);
+}
+</style>
