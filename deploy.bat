4e4f9e62f2bd97e1305c51f64fded809f845cbcd
NightFeather <leolee50910@gmail.com>
2021-11-20 20:27:30 +0800

    Deploy at 20/11/2021 Sat-20:27:25.78

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,1 @@
-start cmd /c "npm run build && rmdir /s /q .\docs && mkdir .\docs && xcopy .\dist .\docs /e /y && git add -A && git commit -m "Deploy" && git push -u origin main"
+start cmd /k "npm run build && rmdir /s /q .\docs && mkdir .\docs && xcopy .\dist .\docs /e /y && git add -A && git commit -m "Deploy at %date%-%time%" && git push -u origin main"
