8b48b88f18ddf5fabe62cc4e6d955403ad94b9bc
NightFeather <leolee50910@gmail.com>
2021-11-20 19:31:44 +0800

    Deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,6 @@ module.exports = {
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
-    fontFamily: {
-      'google-sans-regular': ['Google Sans Regular'],
-      'google-sans-medium': ['Google Sans Medium'],
-      'google-sans-bold': ['Google Sans Bold'],
-    }
   },
   variants: {
     extend: {},
