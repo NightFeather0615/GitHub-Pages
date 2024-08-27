02bcb20d3150a3e74f6b55984f6c782eb722ae79
NightFeather <leolee50910@gmail.com>
2024-08-27 20:37:51 +0800

    s

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,18 +12,12 @@ function return_home_page() {
 
 <template>
   <html>
-    <body style="" class="">
+    <body style="background-color: rgb(21, 24, 29);" class="">
       <div class="flex items-center justify-center h-screen">
-        <div class="">
-          <div class="">
-            <p class="text-9xl tracking-wide inline font-sans font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-500 to-red-600">404</p>
-            <p class="text-9xl tracking-wide inline font-sans text-transparent bg-clip-text text-gray-300"> | </p>
-            <p class="text-9xl tracking-wide inline font-sans font-bold text-transparent bg-clip-text bg-gradient-to-br from-red-500 to-red-600">Not Found</p>
-          </div>
-          <div class="col-start-1 row-start-2 space-x-20 space-y-16 px-10">
-            <button type="button" @click="close_window()" class="inline hover:from-gray-400 hover:to-gray-500 hover:text-gray-900 bg-gradient-to-br from-gray-600 to-gray-700 text-gray-200 text-2xl font-sans font-bold px-20 py-5 rounded-lg">關閉分頁</button>
-            <button type="button" @click="return_home_page()" class="inline hover:from-gray-400 hover:to-gray-500 hover:text-gray-900 bg-gradient-to-br from-gray-600 to-gray-700 text-gray-200 text-2xl font-sans font-bold px-20 py-5 rounded-lg">返回主頁</button>
-          </div>
+        <div class="columns-3 flex md:flex-row gap-x-4 gap-y-4 flex-col-reverse">
+            <img class="rounded-full scale-50" src="https://yt3.ggpht.com/ytc/AKedOLTNqe_YfIPRXautwOlASmqomp320hByntdX5-bEAQ=s512-c-k-c0x00ffffff-no-rj" loading="lazy"></img>
+            <p class="sm:pt-44 text-9xl tracking-wide font-sans text-transparent bg-clip-text text-gray-300" style="text-align: center;">×</p>
+            <img class="rounded-full scale-50" src="https://cdn.discordapp.com/avatars/866699115299864586/2c24f68fb0fd58cc7555ec0ff2f8eac8.webp?size=512" loading="lazy"></img>
         </div>
       </div>
     </body>
